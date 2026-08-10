435ec3facd97edb283aefe688d9b6dd9e703af10
Adityachouk21 <adityabpl2006@gmail.com>
2026-08-10 23:10:51 +0530

    Add files via upload

---
+++ EPAM/Assignment 3/MaximumLearningPoints.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class MaximumLearningPoints {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int N = sc.nextInt();
+
+            int[] points = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                points[i] = sc.nextInt();
+            }
+
+            if (N == 1) {
+                System.out.println(points[0]);
+                return;
+            }
+
+            int[] dp = new int[N];
+
+            // First topic
+            dp[0] = points[0];
+
+            // First two topics
+            dp[1] = Math.max(points[0], points[1]);
+
+            // Remaining topics
+            for (int i = 2; i < N; i++) {
+
+                dp[i] = Math.max(
+                        dp[i - 1],
+                        dp[i - 2] + points[i]
+                );
+            }
+
+            System.out.println(dp[N - 1]);
+            sc.close();
+        }
+    }
+
---
+++ EPAM/Assignment 3/ScholarshipDistribution.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ScholarshipDistribution {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int N = sc.nextInt();
+            long B = sc.nextLong();
+
+            int[] scholarship = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                scholarship[i] = sc.nextInt();
+            }
+
+            // Sort in increasing order
+            Arrays.sort(scholarship);
+
+            long total = 0;
+            int count = 0;
+
+            for (int i = 0; i < N; i++) {
+
+                if (total + scholarship[i] <= B) {
+                    total += scholarship[i];
+                    count++;
+                } else {
+                    break;
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
